88713665f4bc276abbbf098bfc17d02a17b83b7c
elwira-m <36117564+elwira-m@users.noreply.github.com>
2020-02-21 00:35:59 +0100

    Update new-script.js

--- new-script.js
+++ new-script.js
@@ -1,6 +1,50 @@
+$(function(){
+	
+    var prefix = "https://cors-anywhere.herokuapp.com/";
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    /* var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"; */
+    var quoteUrl = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";	
+	
+       
+    function getQuote() {
+        $.getJSON(prefix + quoteUrl, createTweet);
+        $.ajaxSetup({ cache: false });
+    }	
+	
+    function createTweet(input) {
+
+        var data = input[0];
+        var quoteText = $(data.content).text().trim();
+        var quoteAuthor = data.title;
+
+        if (!quoteAuthor.length) {
+            quoteAuthor = "Unknown author";
+        }
+
+        var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+        if (tweetText.length > 280) {	                                            	
+    	  getQuote();				                                                        
+	    } else {
+	  	    var tweet = tweetLink + encodeURIComponent(tweetText);
+	 	    $('.quote').text(quoteText);
+    	    $('.author').text("Author: " + quoteAuthor);
+    	    $('.tweet').attr('href', tweet);
+	    };
+    };
+                                         
+    $(document).ready(function() {
+        getQuote();
+        $('.trigger').click(function() {
+            getQuote();
+        });
+    });
+
+});
+
 ////////////////Random Chuck Norris Fact//////////
 
-$(function(){  
+/*$(function(){  
 
   var url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
   
@@ -21,7 +65,7 @@ $(function(){
 
   getJoke();
 
-});
+});*/
 /*
 var data = null;
 var xhr = new XMLHttpRequest();
